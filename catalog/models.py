565a929281e77f1b30835e469987e62d0fb80ef2
LeBwa <lebwa1337@gmail.com>
2024-02-01 21:54:40 +0200

    add 4 models and set custom user in settings"

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MusicAuthor(AbstractUser):
+    creation_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=100)
+    duration = models.TimeField()
+    author = models.ForeignKey(MusicAuthor, on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre)
+    lyrics = models.TextField()
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name} with duration {self.duration}"
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    tracks = models.ForeignKey(Track, on_delete=models.CASCADE)
+    description = models.TextField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return f"Album '{self.name}' was released on {self.release_date}"
+
+
+
+
+
+
+
--- music_library/settings.py
+++ music_library/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "catalog.MusicAuthor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
