d897eaa5d431fbcf94b8bc8fca586a87103ee07e
LeBwa <lebwa1337@gmail.com>
2024-02-05 19:47:15 +0200

    Transfer MusicAuthorList from catalog app to user app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from catalog.views import index
-from user.views import MusicAuthorListView
 
 urlpatterns = [
     path('', index, name='index'),
-    path("musicauthor/", MusicAuthorListView.as_view(), name='author-list'),
 ]
 
 
--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
-from user.views import MusicAuthorCreateView, MusicAuthorDetailView, MusicAuthorUpdateView, MusicAuthorDeleteView
+from user.views import (
+    MusicAuthorCreateView,
+    MusicAuthorDetailView,
+    MusicAuthorUpdateView,
+    MusicAuthorDeleteView,
+    MusicAuthorListView
+)
 
 urlpatterns = [
     path("musicauthor/create/", MusicAuthorCreateView.as_view(), name='author-create'),
+    path("musicauthor/", MusicAuthorListView.as_view(), name='author-list'),
     path("musicauthor/<int:pk>/detail/", MusicAuthorDetailView.as_view(), name='author-detail'),
     path("musicauthor/<int:pk>/update/", MusicAuthorUpdateView.as_view(), name='author-update'),
-    path("musicauthor/<int:pk>/delete/", MusicAuthorDeleteView.as_view(), name='author-delete')
-
+    path("musicauthor/<int:pk>/delete/", MusicAuthorDeleteView.as_view(), name='author-delete'),
 ]
 
 app_name = 'user'
