a419dc4eca3ead144204ab297a6d843655a4e85f
LeBwa <lebwa1337@gmail.com>
2024-02-14 17:04:05 +0200

    Update settings.Prepare project to deploy

--- music_library/settings.py
+++ music_library/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,14 +19,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cf$p8hrrdk%d_(lqybk5b8&^8am7q*i%bv1*t5!&&+rrvmltkb'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-cf$p8hrrdk%d_(lqybk5b8&^8am7q*i%bv1*t5!&&+rrvmltkb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",]
 
 ASSETS_ROOT = '/static/assets'
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
 
 # Application definition
 
