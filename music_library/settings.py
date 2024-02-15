af603257bc33280c0d0534c98c8323481b796cf4
LeBwa <lebwa1337@gmail.com>
2024-02-15 20:35:22 +0200

    remove secret key for deployment

--- music_library/settings.py
+++ music_library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-cf$p8hrrdk%d_(lqybk5b8&^8am7q*i%bv1*t5!&&+rrvmltkb')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != "False"
