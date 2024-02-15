3c905bdb913799b92e19f21fa04cd588f85117ac
LeBwa <lebwa1337@gmail.com>
2024-02-15 20:57:40 +0200

    Add render url to allowed hosts

--- music_library/settings.py
+++ music_library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != "False"
 
-ALLOWED_HOSTS = ["localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", '127.0.0.1', "music-library-ip0s.onrender.com"]
 
 ASSETS_ROOT = '/static/assets'
 
